1b62fc776932d7eb2b03f45d37c3f36d082cca03
IRFAN CAN <irfancan.atay@starcut.com>
2018-09-18 20:42:06 +0300

    Created basic Recyclerview

--- app/src/main/java/com/irfancan/deliverpad/MainActivity.java
+++ app/src/main/java/com/irfancan/deliverpad/MainActivity.java
@@ -2,16 +2,50 @@ package com.irfancan.deliverpad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.irfancan.deliverpad.adapter.DeliveriesAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RecyclerView mDeliveriesRecyclerView;
+    private RecyclerView.Adapter mDeliveriesAdapter;
+    private RecyclerView.LayoutManager mDeliveriesLayoutManager;
+
+    //Just for testing purpose
+    List<String> myDeliveries=new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Testing my very first commit :)
-        //Second commit test
+        //RecyclerView Ref
+        mDeliveriesRecyclerView = findViewById(R.id.deliveries_recycler_view);
+
+        //Linear Layout
+        mDeliveriesLayoutManager = new LinearLayoutManager(this);
+        mDeliveriesRecyclerView.setLayoutManager(mDeliveriesLayoutManager);
+
+
+        //Just for test purpose
+        myDeliveries.add("HONG KONG");
+        myDeliveries.add("HELSINKI");
+        myDeliveries.add("REYKJAVIK");
+
+
+        //Adapter
+        mDeliveriesAdapter = new DeliveriesAdapter(myDeliveries);
+        mDeliveriesRecyclerView.setAdapter(mDeliveriesAdapter);
 
     }
+
 }
+
