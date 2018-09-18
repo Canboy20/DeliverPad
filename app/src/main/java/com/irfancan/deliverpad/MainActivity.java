1db70efd677ffba67de01e660cc735208c19c928
IRFAN CAN <irfancan.atay@starcut.com>
2018-09-18 20:43:49 +0300

    Created basic Recyclerview

--- app/src/main/java/com/irfancan/deliverpad/MainActivity.java
+++ app/src/main/java/com/irfancan/deliverpad/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         myDeliveries.add("HONG KONG");
         myDeliveries.add("HELSINKI");
         myDeliveries.add("REYKJAVIK");
+        myDeliveries.add("PARIS");
+
 
 
         //Adapter
---
+++ app/src/main/java/com/irfancan/deliverpad/viewholder/DeliveryViewHolder.java
@@ -0,0 +1,27 @@
+package com.irfancan.deliverpad.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.irfancan.deliverpad.R;
+
+public class DeliveryViewHolder extends RecyclerView.ViewHolder {
+
+
+    public LinearLayout mLinearLayout;
+    private TextView mDescriptionTextView;
+
+    public DeliveryViewHolder(LinearLayout v) {
+        super(v);
+        mLinearLayout = v;
+        mDescriptionTextView = v.findViewById(R.id.descriptionTextView);
+    }
+
+
+
+    public TextView getDescriptionTextView(){
+        return mDescriptionTextView;
+    }
+
+}
