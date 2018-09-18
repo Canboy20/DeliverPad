a6e4c9e0aabca867719b9afaf4c853372557aba1
IRFAN CAN <irfancan.atay@starcut.com>
2018-09-18 12:32:23 +0300

    My Very first commit :)

---
+++ app/src/main/java/com/irfancan/deliverpad/MainActivity.java
@@ -0,0 +1,16 @@
+package com.irfancan.deliverpad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Testing my very first commit :)
+
+    }
+}
